cf0e3770274795eeb4aa10379b04931dc3e977c1
Terry Hung <terrymhung@gmail.com>
2023-08-18 09:39:42 -0400

    Add more details to passed tests

--- tests/main.py
+++ tests/main.py
@@ -6,15 +6,19 @@ from tst_login import clear_input_fields
 
 # Test a failed login attempt.
 test_login_with_invalid_credentials()
+print("Attempt login with invalid credentials - Pass")
 clear_input_fields()
 
 # Test with only username.
 test_login_only_username()
+print("Attempt login with only user name - Pass")
 clear_input_fields()
 
 # Test with no credentials.
 test_login_with_no_credentials()
+print("Attempt login with no credentials - Pass")
 
 # Test a successful login attempt.
 test_login_with_valid_credentials()
+print("Login with valid credentials - Pass")
 
